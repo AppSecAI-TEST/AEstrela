96564d14a52a3847aad285cb358a24e180f116c1
guest-tqterz <guest-tqterz@DINSL007-PC06>
2017-06-09 20:53:01 -0300

    Criação da estrutura inicial

---
+++ AEstrelaz/src/aestrelaz/model/No.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aestrelaz.model;
+
+import java.util.List;
+
+/**
+ *
+ * @author guest-tqterz
+ */
+public class No {
+
+    private float funcaoG;
+    private float funcaoHLinha;
+    private float funcaoP;
+
+    private List<No> adjacentes;
+    private No noPai;
+
+    private int tabela[] = new int[16];
+
+    public No() {
+//        Scanner sc1 = new Scanner(System.in);
+//        scan.ne
+
+    }
+
+    public void geraSucessores() {
+
+    }
+
+    public void calculaFuncaoG() {
+
+    }
+
+    public void calculaFuncaoHLinha() {
+
+    }
+
+    public void calculaFuncaoP() {
+
+    }
+
+}
